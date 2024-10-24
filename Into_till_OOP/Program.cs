83466636e0859ce6eb9f0f7b18823c7624726196
TimiSUT24 <tim.petersenfalkenberg@outlook.com>
2024-10-24 14:42:28 +0200

    Add project files.

---
+++ Into_till_OOP/Program.cs
@@ -0,0 +1,38 @@
+﻿
+
+namespace Into_till_OOP
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            var circle = new Circle(5);           
+            Console.WriteLine($"Arean på cirkel 1 är: {circle.GetArea()}");
+
+            var circle2 = new Circle(6); 
+            Console.WriteLine($"Arean på cirkel 2 är: {circle2.GetArea()}");
+
+        }
+        public class Circle
+        {
+             float _pi = 3.141f;
+             int Radius;
+                      
+            public Circle(int radius)
+            {
+                Radius = radius;
+            }
+            
+            public float GetArea()
+            {
+                return Radius * Radius * _pi;
+            }
+            
+
+        }
+
+        
+    }
+
+    
+}
